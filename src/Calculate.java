3aa4fae7b9f6d0289d39e0a357130bc6157987e3
Ritesh <panigrahiritesh03@gmail.com>
2020-09-23 11:50:57 +0530

    Calculator complete test

--- src/Calculate.java
+++ src/Calculate.java
@@ -5,4 +5,15 @@ public class Calculate {
     public int diff(int a,int b){
         return a-b;
     }
+    public int multiply(int a,int b){
+        return a*b;
+    }
+    public float div(int a,int b){
+        if(b==0){
+            throw new ArithmeticException();
+        }
+        return a/b;
+    }
+
+
 }
--- src/TestCalc.java
+++ src/TestCalc.java
@@ -23,6 +23,38 @@ public class TestCalc {
         int expected =-1;
         Assert.assertEquals(expected,actual);
     }
+    @Test
+    public void testMultiply(){
+        Calculate c= new Calculate();
+        System.out.println("Multiply test");
+        int a=4;
+        int b =5;
+        int actual =c.multiply(a,b) ;
+        int expected =20;
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void testDiv(){
+        Calculate c= new Calculate();
+        System.out.println("Division test");
+        int a=10;
+        int b =5;
+        float actual =c.div(a,b);
+        float expected =2;
+        Assert.assertEquals(expected, actual,0.0001);
+    }
+    @Test(expected = ArithmeticException.class)
+    public void testDivWithZeroAsDenominator(){
+        System.out.println("Denominator cannot be zero");
+        int a=10;
+        int b=0;
+        Calculate c= new Calculate();
+        c.div(a,b);
+        System.out.println("Denominator cannot be zero");
+
+
+    }
+
     @Before
     public void runBeforeEveryTest(){
         System.out.println("@before running");
