90be2a6aa6ec524b3a4f5881eb2b9a80ad633cf4
Ritesh <panigrahiritesh03@gmail.com>
2020-09-23 10:26:42 +0530

    Running 2 Test

--- src/Calculate.java
+++ src/Calculate.java
@@ -2,4 +2,7 @@ public class Calculate {
     public int add(int a,int b){
        return a+b;
     }
+    public int diff(int a,int b){
+        return a-b;
+    }
 }
--- src/TestCalc.java
+++ src/TestCalc.java
@@ -1,13 +1,42 @@
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
+
+import java.net.StandardSocketOptions;
+
 public class TestCalc {
     @Test
     public void testSum(){
+        System.out.println("add test");
         Calculate c= new Calculate();
         int a=4;
         int b =5;
         int actual =c.add(a,b) ;
         int expected =9;
         Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void testDiff(){
+        Calculate c= new Calculate();
+        System.out.println("sub test");
+        int a=4;
+        int b =5;
+        int actual =c.diff(a,b) ;
+        int expected =-1;
+        Assert.assertEquals(expected,actual);
+    }
+    @Before
+    public void runBeforeEveryTest(){
+        System.out.println("@before running");
+    }
+    @After
+    public void runAfterEveryTest(){
+        System.out.println("@after running");
+    }
+    @BeforeClass
+    public static void runBeforeClass(){
+        System.out.println("@beforeclass running");
+    }
+    @AfterClass
+    public static void runAfterClass(){
+        System.out.println("@afterclass running");
     }
 }
